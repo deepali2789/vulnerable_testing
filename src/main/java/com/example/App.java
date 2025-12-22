4e65e2635c56b1359a7689c9ceea1f916d206313
deepali2789 <106826864+deepali2789@users.noreply.github.com>
2025-12-22 16:00:35 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("JFrog curation demo project (for vulnerability policy testing).");
+        String input = "jfrog curation demo";
+        System.out.println(StringUtils.capitalize(input));
     }
 }
+
